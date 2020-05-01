374e370a340256a5ef1864a3f08fa5760ba0a7f3
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-01 17:25:28 +0300

    drcc1d2

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6E4VdA1UzHN":
+        Script1();
+        break;
+      case "5htZA0AJyL0":
+        Script2();
+        break;
+      case "6pSH53NrPKV":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
